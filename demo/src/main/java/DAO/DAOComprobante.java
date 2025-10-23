e5dc516b400e122087eb7ec25b6796cb561d3a6c
Dan-1395 <astohuamanepsinodaniela@gmail.com>
2025-10-22 19:23:04 -0500

    a

--- demo/src/main/java/DAO/DAOComprobante.java
+++ demo/src/main/java/DAO/DAOComprobante.java
@@ -1,4 +1,5 @@
 package DAO;
+import java.time.LocalDate;
 import java.util.List;
 import DTO.Comprobante;
 
@@ -7,4 +8,9 @@ public interface DAOComprobante {
     public void eliminarComprobante(Comprobante comp);
     public List<Comprobante> listarComprobante();
     public Comprobante buscarComprobante(Comprobante comp);
+    public List<Comprobante> filtrarComprovanteHoy (LocalDate hoy);
+    public List<Comprobante> filtrarComprobantesSemana();
+    public List<Comprobante> filtrarComprobantePorMes(int año, int mes);
+    public List<Comprobante> filtrarComprobantePorAño(int año);
+    public List<Comprobante> filtrarComprobantePorCliente(Long clienteId);
 }
--- demo/src/main/java/DAOImpl/DAOComprobanteImpl.java
+++ demo/src/main/java/DAOImpl/DAOComprobanteImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,11 @@ public class DAOComprobanteImpl implements DAOComprobante{
     private static final String SQLeliminar="Delete from Comprobante where idComprobante = ?";
     private static final String SQLbuscar="Select * from Comprobante where idComprobante = ?";
     private static final String SQLlista="Select * from Comprobante";
+    private static final String SQLfiltroHoy="Select * from comprobante where fechaemision = ? ";
+    private static final String SQLfiltroSemana="Select * from comprobante where fechaemision >= ? and fechaemision <= ? ";
+    private static final String SQLfiltroMes="Select * from comprobante where extract(year from fechaemision) = ? and extract(month from fechaemision) = ?";
+    private static final String SQLfiltroAño="Select * from comprobante where extract(year from fechaemision) = ? ";
+    private static final String SQLfiltroPorCliente="Select * from comprobante where idcliente = ? ";
 
     @Override
     public void registarComprobante(Comprobante comp) {
@@ -59,8 +65,8 @@ public class DAOComprobanteImpl implements DAOComprobante{
     public List<Comprobante> listarComprobante() {
         List<Comprobante> listaComprobante = new ArrayList<>();
         try (Connection conn = ConexionBD.getInstance().getConnection();
-             PreparedStatement ps = conn.prepareStatement(SQLlista);
-             ResultSet res = ps.executeQuery()) {            
+            PreparedStatement ps = conn.prepareStatement(SQLlista);
+            ResultSet res = ps.executeQuery()) {            
             while (res.next()) {
                 Comprobante co = new Comprobante();
                 co.setIdComprobante(res.getLong("idComprobante"));
@@ -109,4 +115,147 @@ public class DAOComprobanteImpl implements DAOComprobante{
         return co;
     }
 
+    public List<Comprobante> filtrarComprovanteHoy (LocalDate hoy){
+        List<Comprobante> comprobanteHoy= new ArrayList<>();
+        try {
+            Connection conn=ConexionBD.getInstance().getConnection();
+            PreparedStatement ps= conn.prepareStatement(SQLfiltroHoy);
+            ps.setDate(1, java.sql.Date.valueOf(hoy));
+            ResultSet res=ps.executeQuery();
+            while (res.next()) {
+                Comprobante co = new Comprobante();
+                co.setIdComprobante(res.getLong("idComprobante"));
+                co.setFechaEmision(res.getDate("FechaEmision").toLocalDate());
+                co.setSerie(res.getString("numSerie"));
+                co.setTotalFinal(res.getBigDecimal("TotalFinal"));
+                co.setDireccionEnvio(res.getString("direccionEnvio"));
+                co.setDevengado(res.getBigDecimal("devengado"));
+                co.setIdMedioPago(res.getLong("idMedioPago"));
+                co.setIdCliente(res.getLong("idCliente"));
+                co.setIdUsuario(res.getLong("idUsuario"));
+                co.setIdTipoComprobante(res.getLong("idTipoComprobante"));
+                comprobanteHoy.add(co);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al listar comprobantes: " + e.getMessage(), e);
+        }        return comprobanteHoy ;
+    }
+
+    public List<Comprobante> filtrarComprobantesSemana(){
+        List<Comprobante> comprobanteSemana= new ArrayList<>();
+        try {
+            Connection conn=ConexionBD.getInstance().getConnection();
+            PreparedStatement ps= conn.prepareStatement(SQLfiltroSemana);
+            LocalDate hoy = LocalDate.now();
+            LocalDate Semana= hoy.minusDays(7);
+            ps.setDate(1, java.sql.Date.valueOf(Semana));
+            ps.setDate(2, java.sql.Date.valueOf(hoy));
+
+            ResultSet res=ps.executeQuery();
+            while (res.next()) {
+                Comprobante co = new Comprobante();
+                co.setIdComprobante(res.getLong("idComprobante"));
+                co.setFechaEmision(res.getDate("FechaEmision").toLocalDate());
+                co.setSerie(res.getString("numSerie"));
+                co.setTotalFinal(res.getBigDecimal("TotalFinal"));
+                co.setDireccionEnvio(res.getString("direccionEnvio"));
+                co.setDevengado(res.getBigDecimal("devengado"));
+                co.setIdMedioPago(res.getLong("idMedioPago"));
+                co.setIdCliente(res.getLong("idCliente"));
+                co.setIdUsuario(res.getLong("idUsuario"));
+                co.setIdTipoComprobante(res.getLong("idTipoComprobante"));
+                comprobanteSemana.add(co);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al listar comprobantes: " + e.getMessage(), e);
+        }   
+        return comprobanteSemana;
+    }
+
+    public List<Comprobante> filtrarComprobantePorMes(int año, int mes){
+        List<Comprobante> comprobanteMes= new ArrayList<>();
+        try {
+            Connection conn=ConexionBD.getInstance().getConnection();
+            PreparedStatement ps= conn.prepareStatement(SQLfiltroMes);
+            ps.setInt(1, año);
+            ps.setInt(2, mes);
+            ResultSet res=ps.executeQuery();
+            while (res.next()) {
+                Comprobante co = new Comprobante();
+                co.setIdComprobante(res.getLong("idComprobante"));
+                co.setFechaEmision(res.getDate("FechaEmision").toLocalDate());
+                co.setSerie(res.getString("numSerie"));
+                co.setTotalFinal(res.getBigDecimal("TotalFinal"));
+                co.setDireccionEnvio(res.getString("direccionEnvio"));
+                co.setDevengado(res.getBigDecimal("devengado"));
+                co.setIdMedioPago(res.getLong("idMedioPago"));
+                co.setIdCliente(res.getLong("idCliente"));
+                co.setIdUsuario(res.getLong("idUsuario"));
+                co.setIdTipoComprobante(res.getLong("idTipoComprobante"));
+                comprobanteMes.add(co);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al listar comprobantes: " + e.getMessage(), e);
+        } 
+        return comprobanteMes;
+    }
+    public List<Comprobante> filtrarComprobantePorAño(int año){
+        List<Comprobante> comprobanteAño= new ArrayList<>();
+        try {
+            Connection conn=ConexionBD.getInstance().getConnection();
+            PreparedStatement ps= conn.prepareStatement(SQLfiltroAño);
+            ps.setInt(1, año);
+            ResultSet res=ps.executeQuery();
+            while (res.next()) {
+                Comprobante co = new Comprobante();
+                co.setIdComprobante(res.getLong("idComprobante"));
+                co.setFechaEmision(res.getDate("FechaEmision").toLocalDate());
+                co.setSerie(res.getString("numSerie"));
+                co.setTotalFinal(res.getBigDecimal("TotalFinal"));
+                co.setDireccionEnvio(res.getString("direccionEnvio"));
+                co.setDevengado(res.getBigDecimal("devengado"));
+                co.setIdMedioPago(res.getLong("idMedioPago"));
+                co.setIdCliente(res.getLong("idCliente"));
+                co.setIdUsuario(res.getLong("idUsuario"));
+                co.setIdTipoComprobante(res.getLong("idTipoComprobante"));
+                comprobanteAño.add(co);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al listar comprobantes: " + e.getMessage(), e);
+        } 
+        return comprobanteAño;
+    }
+    public List<Comprobante> filtrarComprobantePorCliente(Long clienteId){
+        List<Comprobante> comprobanteCliente= new ArrayList<>();
+        try {
+            Connection conn=ConexionBD.getInstance().getConnection();
+            PreparedStatement ps= conn.prepareStatement(SQLfiltroPorCliente);
+            ps.setLong(1, clienteId);
+            ResultSet res=ps.executeQuery();
+            while (res.next()) {
+                Comprobante co = new Comprobante();
+                co.setIdComprobante(res.getLong("idComprobante"));
+                co.setFechaEmision(res.getDate("FechaEmision").toLocalDate());
+                co.setSerie(res.getString("numSerie"));
+                co.setTotalFinal(res.getBigDecimal("TotalFinal"));
+                co.setDireccionEnvio(res.getString("direccionEnvio"));
+                co.setDevengado(res.getBigDecimal("devengado"));
+                co.setIdMedioPago(res.getLong("idMedioPago"));
+                co.setIdCliente(res.getLong("idCliente"));
+                co.setIdUsuario(res.getLong("idUsuario"));
+                co.setIdTipoComprobante(res.getLong("idTipoComprobante"));
+                comprobanteCliente.add(co);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al listar comprobantes: " + e.getMessage(), e);
+        }
+        return comprobanteCliente;
+    }
 }
